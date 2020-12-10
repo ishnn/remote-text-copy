bf604525b84cd77a2397b25c9e518329a5cd1bb7
Ishan Agrawal <pyvisualiser@gmail.com>
2020-12-10 20:57:44 +0530

    Update server.py

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ from flask import Flask, request, render_template
 import pyperclip
 
 app = Flask(__name__)
-@app.route('/',methods = ['POST'])
+@app.route('/',methods = ['POST', 'GET'])
 def index():
    if request.method == 'POST':
       user = request.form['nm']
