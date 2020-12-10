f460a37b0599050733d7ec4a4be343266b2fe8ce
Ishan Agrawal <pyvisualiser@gmail.com>
2020-12-10 20:19:22 +0530

    Create server.py

---
+++ server.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, render_template
+import pyperclip
+
+app = Flask(__name__)
+@app.route('/',methods = ['POST'])
+def index():
+   if request.method == 'POST':
+      user = request.form['nm']
+      pyperclip.copy(str(user))
+      return render_template('index.html')
+   else:
+      return render_template('index.html')
+if __name__ == '__main__':
+   app.run(host = '<Your IP address>', port=5000)
